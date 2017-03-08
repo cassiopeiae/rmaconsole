d62d4acd01a4354a61e35885ff31066b65361094
Steve Luzynski <steve@luzynski.net>
2017-03-08 08:55:06 -0600

    linted

--- app.py
+++ app.py
@@ -8,12 +8,8 @@ __author__ = 'sluzynsk'
 
 from flask import Flask
 from flask import render_template
-from flask import redirect
-from flask import url_for
-from flask import jsonify
 from flask import request
 import requests
-import json
 import os
 
 
@@ -21,6 +17,7 @@ app = Flask(__name__)
 
 client_id = os.environ.get("CLIENT_ID")
 client_secret = os.environ.get("CLIENT_SECRET")
+
 
 @app.route('/')
 def home():
@@ -31,23 +28,25 @@ def home():
         for index in rma_num:
             print index
             req_url = 'https://api.cisco.com/return/v1.0/returns/rma_numbers/' + index
-            headers = {'Authorization': 'Bearer ' + get_token() }
-            data = requests.get(req_url,headers=headers)
+            headers = {'Authorization': 'Bearer ' + get_token()}
+            data = requests.get(req_url, headers=headers)
             returns.append(data.json()[u'returns'][u'RmaRecord'][0])
     return render_template('index.html', rma=rma_num, response=returns,
-                               title='RMA Console')
+                           title='RMA Console')
+
 
 def get_token():
     req_url = 'https://cloudsso.cisco.com/as/token.oauth2'
-    headers = {'client_id':client_id,
-               'client_secret':client_secret,
-               'grant_type':'client_credentials'}
+    headers = {'client_id': client_id,
+               'client_secret': client_secret,
+               'grant_type': 'client_credentials'}
     r = requests.post(req_url, params=headers, verify=False)
 
-    if (r.status_code==200):
+    if (r.status_code == 200):
         return r.json()[u'access_token']
     else:
         return 0
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
